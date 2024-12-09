04182930afe0bff4995b8d7a61f4b03e13d96a6a
Andre Tardelli <andre.ltardelli@gmail.com>
2024-12-09 20:33:19 +0000

    Primeiro commit

---
+++ app/src/main/java/com/devspace/taskbeats/CategoryDao.kt
@@ -0,0 +1,11 @@
+package com.devspace.taskbeats
+
+import androidx.room.Dao
+import androidx.room.Query
+
+@Dao
+interface CategoryDao {
+
+    @Query("Select * From categoryentity")
+    fun getAll():List<CategoryEntity>
+}
