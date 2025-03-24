5e829a2f60b00fcd8a2b505b11e89efb0da39bdb
Andre Tardelli <andre.ltardelli@gmail.com>
2025-03-24 19:56:25 +0000

    Retorno as aulas do projeto

--- app/src/main/java/com/devspace/taskbeats/MainActivity.kt
+++ app/src/main/java/com/devspace/taskbeats/MainActivity.kt
@@ -117,7 +117,32 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-    val categories: List<CategoryUiData> = listOf()
+val categories = listOf(
+    CategoryUiData(
+        name = "ALL",
+        isSelected = false
+    ),
+    CategoryUiData(
+        name = "STUDY",
+        isSelected = false
+    ),
+    CategoryUiData(
+        name = "WORK",
+        isSelected = false
+    ),
+    CategoryUiData(
+        name = "WELLNESS",
+        isSelected = false
+    ),
+    CategoryUiData(
+        name = "HOME",
+        isSelected = false
+    ),
+    CategoryUiData(
+        name = "HEALTH",
+        isSelected = false
+    ),
+)
 
     val tasks = listOf(
         TaskUiData(
