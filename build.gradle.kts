37839241fdfb35fda8cf6b42ad1a07e424059817
Andre Tardelli <andre.ltardelli@gmail.com>
2025-03-29 15:15:57 +0000

    Resolvendo conflito no build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-<<<<<<< HEAD
-    id("com.android.application") version "8.2.2" apply false
-=======
     id("com.android.application") version "8.5.0" apply false
->>>>>>> 5e829a2f60b00fcd8a2b505b11e89efb0da39bdb
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
